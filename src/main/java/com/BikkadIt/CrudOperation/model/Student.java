20616e9b79080a73c7004081c005d77291430a89
SHEKHAR240196 <shekharpatle999@gmail.com>
2024-06-20 17:22:43 +0530

    v1:C-12345 Create fisrt project

---
+++ src/main/java/com/BikkadIt/CrudOperation/model/Student.java
@@ -0,0 +1,29 @@
+package com.BikkadIt.CrudOperation.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  int id;
+
+    private String name;
+
+    private String email;
+
+    private String city;
+
+    private String age;
+
+}
